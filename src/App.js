f84a187d021599b96e5ebf3693420334ef38faa0
prashantatedu <prashantgit2021@gmail.com>
2024-05-13 16:19:35 +0530

    small change

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Welcome to React</h1>
+        <h1>Welcome to React JS</h1>
       </header>
     </div>
   );
